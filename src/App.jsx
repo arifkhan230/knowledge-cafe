96bacfa8c7834796859b115e838f6a5037b09eb2
arifkhan101 <103602241+arifkhan101@users.noreply.github.com>
2023-09-12 14:14:49 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Blogs from './assets/components/Blogs/Blogs'
+import Header from './assets/components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
